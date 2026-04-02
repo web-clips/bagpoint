69476a8ab6a364b6c3fbe612d1422554f0256d67
web-clips <azamatmaxout@gmail.com>
2026-04-03 00:08:09 +0500

    style

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import CategoryPage from './pages/CategoryPage';
 import FavoritesPage from './pages/FavoritesPage';
 import CartPage from './pages/CartPage';
 import ProductPage from './pages/ProductPage';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="app-shell">
+      <ScrollToTop />
       <Header />
       <main className="container main-content">
         <Routes>
